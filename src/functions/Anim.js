46472c46ed43729dde0e1770349a9a289ae4cd5c
Fernufur <mkgpena@gmail.com>
2026-05-04 13:41:34 -0400

    reverted layout a bit
    removed the grid layout because I couldn't get the menu to work with it yet

--- src/functions/Anim.js
+++ src/functions/Anim.js
@@ -1,5 +1,14 @@
+window.onscroll = function(){scrollHeaderFunc(),scrollBarFunc()};
 
-window.onscroll = function(){scrollBarFunc()};
+function scrollHeaderFunc(){
+  if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+    document.getElementById("header").style.height = "4em";
+    document.getElementById("logo").style.fontSize = "25px";
+  }else{
+    document.getElementById("header").style.height = "7em";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
 function scrollBarFunc(){
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
---
+++ src/functions/Util.js
@@ -0,0 +1,3 @@
+const root = document.documentElement;
+root.style.setProperty('--header-height');
+root.style.setProperty('--scroll-header-height');
