d62e7b2356350880933f8f56e6f93e0c107e8e83
Fernufur <mkgpena@gmail.com>
2026-05-12 12:00:25 -0400

    yo

--- src/functions/Util.js
+++ src/functions/Util.js
@@ -1,11 +1,3 @@
 const root = document.documentElement;
 root.style.setProperty('--header-height');
 root.style.setProperty('--scroll-header-height');
-
-/*
-        __  ___          __     
-       /  |/  /___  ____/ /____
-      / /|_/ / __ \/ __  / ___/ 
-     / /  / / /_/ / /_/ (__  )  
-    /_/  /_/\____/\__,_/____/  
-*/
